1669c7fda0c8123bcc6aaf9be9265f4dfcc9335c
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-21 15:23:38 -0300

    feat: add database connection functionality

--- database_interface.py
+++ database_interface.py
@@ -1,2 +1,12 @@
 from tkinter import *
 from dotenv import load_dotenv
+import mysql.connector
+import os
+
+load_dotenv()
+
+bd_connection = mysql.connector.connect(host=os.getenv("HOST"),
+                                        user=os.getenv("USER"),
+                                        password=os.getenv("PASSWORD"),
+                                        database=os.getenv("DATABASE_NAME"))
+
