d300de7d8130591ac20890c67f16ad38fc5f5f3d
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-21 16:03:42 -0300

    feat: create script for database insert function

--- database_interface.py
+++ database_interface.py
@@ -3,12 +3,17 @@ from dotenv import load_dotenv
 import mysql.connector
 import os
 
+from insert_orders import insert_orders
+
 load_dotenv()
 
 bd_connection = mysql.connector.connect(host=os.getenv("HOST"),
                                         user=os.getenv("USER"),
                                         password=os.getenv("PASSWORD"),
                                         database=os.getenv("DATABASE_NAME"))
+
+
+cursor = bd_connection.cursor()
 
 main_window = Tk()
 main_window.title("Interface Banco de Dados")
@@ -18,7 +23,7 @@ main_window.resizable(width=False, height=False)
 label_welcome = Label(main_window, text="Bem Vindo(a)", width=10, font=("Arial", 12, "bold"))
 label_welcome.place(x=90, y=20)
 
-insert_order_btn = Button(main_window, text="Inserir Encomenda", width=16, bg="green", fg="white")
+insert_order_btn = Button(main_window, text="Inserir Encomenda", command=lambda: insert_orders(cursor, bd_connection), width=16, bg="green", fg="white")
 insert_order_btn.place(x=40, y=90)
 
 insert_expense_btn = Button(main_window, text="Inserir Despesa", width=12, bg="green", fg="white")
---
+++ insert_orders.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+def insert_orders(cursor, bd_connection):
+    def insert_to_bd():
+        client_name = entries[0].get()
+        quantity = entries[1].get()
+        product_name = entries[2].get()
+        product_value = entries[3].get()
+        promotion = entries[4].get()
+        payment_method = entries[5].get()
+        delivery_type = entries[6].get()
+
+        insert_query = """INSERT INTO encomendas (s_nomedocliente, i_quantidade, s_nomedoproduto, 
+                            f_valordoproduto, s_promocao, s_formadepagamento, s_tipodeentrega)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        values = (client_name, quantity, product_name, product_value, promotion,  payment_method, delivery_type)
+            
+        cursor.execute(insert_query, values)
+        bd_connection.commit()  
+        print("Registro inserido com sucesso")
+
+    order_window = Toplevel()
+    order_window.title("Inserir Registro de Encomenda")
+    order_window.geometry("400x400")
+    order_window.resizable(width=False, height=False)
+
+    label_title = Label(order_window, text="Insira um registro", width=20, font=("Arial", 12, "bold"))
+    label_title.place(x=90, y=20)
+
+    labels = ["Nome do Cliente:", "Quantidade:", "Nome do Produto:", 
+              "Valor do Produto:", "Promoção (Sim/Não):", 
+              "Forma de Pagamento:", "Tipo de Entrega:"]
+
+    entries = []
+    for i, label_text in enumerate(labels):
+        label = Label(order_window, text=label_text)
+        label.place(x=20, y=60 + i * 30) 
+        entry = Entry(order_window)
+        entry.place(x=180, y=60 + i * 30, width=200)
+        entries.append(entry)
+
+    button_save = Button(order_window, text="Salvar Encomenda", command=insert_to_bd)
+    button_save.place(x=150, y=300)
+
